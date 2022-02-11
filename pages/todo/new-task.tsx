869654a1066d97ebf57cb4628b30a76f20ae0b76
ShekMak <majorsonshekinah@gmail.com>
2022-02-11 20:51:39 +0200

    New Todo Page Design

---
+++ pages/todo/new-task.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+import React from 'react'
+import NewTodo from '../../components/NewTodo';
+
+const Newtask: NextPage = () => {
+  return (
+    <NewTodo />
+  );
+}
+
+export default Newtask;
