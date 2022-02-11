daf8fc6ecf8644429cc0cde990ec56cdfcb17dd5
ShekMak <majorsonshekinah@gmail.com>
2022-02-11 20:46:43 +0200

    Replacing svg by boostrap icon and modifying layout

--- components/Todos.tsx
+++ components/Todos.tsx
@@ -5,46 +5,66 @@ import Todo from './Todo';
 
 function Todos() {
   const [todos, setTodos] = useState<string[]>([]);
+  const activeStyle = 'relative before:border-b-4 before:absolute before:inset-0 before:h-full before:pt-7 before:border-black';
+  const [tab, setTab] = useState<string>('In Progress');
+  
   const todo: any = {
     complete: false,
     id: 'uid-todo'
   }
 
   useEffect(() => {
-    console.log(todos)
-  },[]);
+    console.log('Rafraichit');
+  }, []);
+
+  useEffect(() => {
+    console.log(tab)
+  },[tab]);
+
   return (
-    <div className='flex flex-col h-full p-4'>
-      <p className='font-normal text-4xl px-6 pt-2 mb-2 flex'>
-        <span>Home</span>
+    <div className='flex flex-col h-full md:w-1/2 md:m-auto p-4'>
+      <i className='bi bi-arrow-up-right-square-fill self-end text-2xl cursor-pointer'></i>
+      <p className='font-normal text-4xl px-6 pt-2 flex justify-center items-center'>
+        <span>todos</span>
       </p>
+      <div className='flex justify-between mt-3 mx-6 py-2 border-b cursor-pointer'>
+        <p className={ tab === 'In Progress' ? `mr-10 px-4 ${activeStyle}` : 'mr-10 px-4'}
+          onClick={ () => setTab('In Progress')}
+        >
+          In progress
+        </p>
+        <p className={ tab === 'Done' ? `px-4 ${activeStyle}` : 'px-4' }
+          onClick={ () => setTab('Done')}
+        >
+          Done
+        </p>
+      </div>
       <div className='flex flex-col h-full overflow-scroll'>
-        {(todos.length === 0) && 
-        <div className="px-6 pt-6">
-          <Todo todo={todo}/>
-          <Todo todo={todo}/>
-          <Todo todo={{...todo, complete: true}}/>
-          <Todo todo={todo}/>
-          <Todo todo={todo}/>
-          <Todo todo={todo}/>
-          <Todo todo={{...todo, complete: true}}/>
-          <Todo todo={{...todo, complete: true}}/>
-          <Todo todo={todo}/>
-          <Todo todo={todo}/>
-          <Todo todo={todo}/>
-          <Todo todo={todo}/>
-          <Todo todo={todo}/>
-          <Todo todo={todo}/>
-          <Link href="/todo/new-task">
-            <a className='h-12 w-full flex items-center justify-center bg-black text-white rounded-md'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="mr-2" viewBox="0 0 16 16">
-                <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
-              </svg>
-              <span className='capitalize'>Add new task</span>
-            </a>
-          </Link>
-        </div>}
-        {(todos.length > 0) && <EmptyTodo/>}
+        {(todos.length > 0) && 
+          <div className="px-6 pt-3">
+            <Todo todo={todo}/>
+            <Todo todo={todo}/>
+            <Todo todo={{...todo, complete: true}}/>
+            <Todo todo={todo}/>
+            <Todo todo={todo}/>
+            <Todo todo={todo}/>
+            <Todo todo={{...todo, complete: true}}/>
+            <Todo todo={{...todo, complete: true}}/>
+            <Todo todo={todo}/>
+            <Todo todo={todo}/>
+            <Todo todo={todo}/>
+            <Todo todo={todo}/>
+            <Todo todo={todo}/>
+            <Todo todo={todo}/>
+            <Link href="/todo/new-task">
+              <a className='h-12 w-full flex items-center justify-center bg-black text-white rounded-md'>
+                <i className='bi bi-plus-lg mr-2'></i>
+                <span className='capitalize'>Add new task</span>
+              </a>
+            </Link>
+          </div>
+        }
+        {(todos.length === 0) && <EmptyTodo/>}
       </div>
     </div>
   );
