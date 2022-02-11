4fb608dde168aee35cf08d337c3614dc7a5b8685
ShekMak <majorsonshekinah@gmail.com>
2022-02-11 20:50:56 +0200

    Adding css layout for all the pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="bg-white text-black font-serif h-screen w-screen overflow-hidden">
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
