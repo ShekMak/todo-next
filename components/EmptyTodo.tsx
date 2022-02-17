694054c7da329e55874aa3c33301f1f2143e2266
ShekMak <majorsonshekinah@gmail.com>
2022-02-17 16:04:14 +0200

    Updating EmptyTodo for Tasks in progress and Tasks done

--- components/EmptyTodo.tsx
+++ components/EmptyTodo.tsx
@@ -1,8 +1,9 @@
 import { Player } from '@lottiefiles/react-lottie-player';
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function EmptyTodo() {
+function EmptyTodo({ tab }: any) {
+  
   return (
     <div className='h-full flex flex-col justify-center items-center md:w-1/2 md:m-auto'>
       <Player 
@@ -11,13 +12,16 @@ function EmptyTodo() {
         src="https://assets6.lottiefiles.com/packages/lf20_gctatsbh.json"
         style={{ height: '300px', width: '300px' }}
         ></Player>
-      <p className='align-middle text-center mt-4 mb-6 px-6'>It seems you have no task, create one and change the world.</p>
-      <Link href="/todo/new-task">
-        <a className='h-12 w-52 flex items-center justify-center bg-black text-white rounded-md'>
-          <i className='bi bi-plus-lg mr-2'></i>
-          <span className='capitalize'>Add new task</span>
-        </a>
-      </Link>
+      <p className='align-middle text-center mt-4 mb-6 px-6'>{tab.message}</p>
+      {
+        tab.title === 'In Progress' && 
+        <Link href="/todo/new-task">
+          <a className='h-12 w-52 flex items-center justify-center bg-black text-white rounded-md'>
+            <i className='bi bi-plus-lg mr-2'></i>
+            <span className='capitalize'>Add new task</span>
+          </a>
+        </Link>
+      }
     </div>
   );
 }
