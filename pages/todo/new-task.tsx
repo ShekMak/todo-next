512f1e38ad71e2b187ebcfa344f5875d4345c85b
ShekMak <majorsonshekinah@gmail.com>
2022-02-18 13:35:03 +0200

    Updating for to check authentification

--- pages/todo/new-task.tsx
+++ pages/todo/new-task.tsx
@@ -1,10 +1,13 @@
 import { NextPage } from 'next';
 import React from 'react'
+import AuthCheck from '../../components/AuthCheck';
 import NewTodo from '../../components/NewTodo';
 
 const Newtask: NextPage = () => {
   return (
-    <NewTodo />
+    <AuthCheck>
+      <NewTodo />
+    </AuthCheck>
   );
 }
 
