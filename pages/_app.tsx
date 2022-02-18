389575a1367773b56c8322c79947c88f6416ad09
ShekMak <majorsonshekinah@gmail.com>
2022-02-18 13:35:38 +0200

    Providing Context for all the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { AuthContextProvider } from '../store/auth.context';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="bg-white text-black font-serif h-screen w-screen overflow-hidden">
+    <AuthContextProvider>
+      <div className="bg-white text-black font-serif h-screen w-screen overflow-hidden">
       <Component {...pageProps} />
     </div>
+    </AuthContextProvider>
   )
 }
 
