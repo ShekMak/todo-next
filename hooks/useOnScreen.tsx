9c5f9d641a65f52c95be93a0e3237f8fbf496682
ShekMak <majorsonshekinah@gmail.com>
2022-02-18 12:21:11 +0200

    CustomHook for to detect visibility of a HTMLElement

---
+++ hooks/useOnScreen.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+
+function useOnScreen(ref: any, rootMargin = '0px') {
+    const [isVisible, setIsVisible] = useState<boolean | string>(false);
+
+    useEffect(() => {
+      if ('IntersectionObserver' in window) {
+        const observer = new IntersectionObserver(
+          ([entry]) => {
+            setIsVisible(entry.isIntersecting);
+          },
+          {
+            rootMargin,
+            threshold: 0.75,
+          }
+        )
+
+        const currentElement = ref?.current;
+
+        if (currentElement) {
+          observer.observe(currentElement);
+        }
+    
+        return () => {
+          observer.unobserve(currentElement);
+        };
+      }else {
+        setIsVisible("Navigator doesn't support IntersectionObserver")
+      }
+    }, [])
+
+    return isVisible;
+}
+
+export default useOnScreen
