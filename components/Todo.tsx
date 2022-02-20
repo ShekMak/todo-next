d0b91e9f11a48b0b256237ead607237fef26ce65
ShekMak <majorsonshekinah@gmail.com>
2022-02-20 12:04:04 +0200

    Implementation of Todo Component

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,18 +1,29 @@
 import Link from 'next/link';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Task from '../models/Task';
 
-function Todo({todo}: any) {
+type IProps = {
+  todo: Task,
+  currentDate: Date
+}
+
+function Todo({todo, currentDate}: IProps) {
 
   const todoStyle = 'border rounded-md shadow-sm py-4 px-4 mb-4 last:mb-0 flex items-center';
-  const overdue = 'border-red'
+  const [overdue, setOverdue] = useState('');
 
   useEffect(() => {
-  }, [todo]);
+    const todoDate = new Date(todo.dueAt?.seconds * 1000);
+    
+    if(currentDate.getTime() >= todoDate.getTime()){
+      setOverdue(' border-red-500');
+    }
+  }, []);
 
   return (
-    <p className={ todo.complete ? `${todoStyle} bg-[#BAC1C9] ${overdue}` : todoStyle}>
+    <p className={ todo.complete ? `${todoStyle} bg-[#BAC1C9]` : todoStyle.concat(overdue)}>
       <span className='mx-2 truncate w-11/12'>
-          TodoTodoTodoTodoTodoTodoTodoTodoTodoTodoTodoTodo
+          {todo.title}
       </span>
       <Link href={`/todo/detail/${todo.id}`}>
         <a>
